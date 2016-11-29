1b2a9d7f91686ef869bf96f5c55a6a25ae36983b
WSS-MASTER\dt210854 <dnamjai@dstworldwideservices.com>
2016-11-29 08:41:09 +0700

    Using js criteria on Report

--- src/app/service/data-transfer-object.ts
+++ src/app/service/data-transfer-object.ts
@@ -1,3 +1,5 @@
+import {formatDate} from '../utils/utility-functions';
+
 export class Menu {
     eventClass: string;
     sectionDesc: string;
@@ -12,8 +14,8 @@ export class Customer {
 }
 
 export class RunMonth {
-    monthId: Number;
-    year: Number;
+    monthId: string;
+    year: string;
 }
 
 export class DateRange {
@@ -24,14 +26,38 @@ export class DateRange {
 export class CriteriaRpt {
     reportNm: string;
     customerId: string;
+    customerName: string;
     fromDt: string;
     toDt: string;
     runMonth: string;
+    runYear: string;
+    reportType: string;
+    facilityCd: string;
+    reportRange: string;
+
+    initial(reportNm, customer: Customer, dateRange: DateRange,runMonthsSelected: RunMonth, reportType, facilityCd, reportRange){
+        this.fromDt = dateRange.fromDt;             this.toDt = dateRange.toDt;   
+        this.runMonth = runMonthsSelected.monthId;  this.runYear = runMonthsSelected.year;
+        this.reportRange = reportRange;             this.reportNm = reportNm;    
+        this.customerId = customer.customerId;      this.customerName = customer.customerName;  
+        this.reportType = reportType;               this.facilityCd = facilityCd;              
+    }
 }
 
 export class Report {
     reportId: string;
-    reportNm: string;
+    criteriaRpt: CriteriaRpt;
+    generateOn: string;
     lastChecked: string;
     status: string;
+
+    initial(reportId, reportNm, criteriaRpt, status){
+        var date = new Date();
+        var generateOn = formatDate(date, 'yyyymmddhhmmss');
+        var lastChecked = formatDate(date, 'MM/dd/yyyy HH:mm:ss');
+
+        this.reportId = reportId;           this.criteriaRpt = criteriaRpt;     
+        this.generateOn = generateOn;       this.lastChecked = lastChecked;     
+        this.status = status;
+    }
 }
--- src/app/service/data.ts
+++ src/app/service/data.ts
@@ -1,8 +1,3 @@
 import {Menu, Customer, Report} from './data-transfer-object';
 
-
-export const REPORTS: Report[] = [
-{"reportId":"1","reportNm":"CASS Mail Report","lastChecked":"11/21/2016 00:04:44", "status":"success"},
-{"reportId":"2","reportNm":"CASS Mail Report","lastChecked":"11/21/2016 06:04:44", "status":"pending"},
-{"reportId":"3","reportNm":"CASS Mail Report","lastChecked":"11/21/2016 12:04:44", "status":"fail"}
-];
+//9-pending, 0-processing, 1-complete, 8-paused, 3-failed
